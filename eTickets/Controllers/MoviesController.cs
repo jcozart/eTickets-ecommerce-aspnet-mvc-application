b1a531bdb2dff0a0ef5cfa2828e7244334a87149
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2023-04-18 14:04:38 -0500

    Updating MoviesController to include Cinemas and order by movie name. Also updated Movies view

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -15,7 +15,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var movies = await _context.Movies.ToListAsync();
+            var movies = await _context.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
             return View(movies);
         }
     }
