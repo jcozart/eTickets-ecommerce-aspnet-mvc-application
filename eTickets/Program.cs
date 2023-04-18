0f614c887dec13312141c7ec8ad4d772d1f88fd6
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2023-04-18 11:50:06 -0500

    Adding migrations and data seeding

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,9 +1,10 @@
 using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -27,4 +28,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+AppDbInitializer.Seed(app);
 app.Run();
